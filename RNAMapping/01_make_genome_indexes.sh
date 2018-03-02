2a80f7d580c86c11ab70dfbfbce1a1c2d1f7a17f
Rob <schae234@umn.edu>
2018-03-02 16:15:24 -0600

    include a GFF file in index

--- RNAMapping/01_make_genome_indexes.sh
+++ RNAMapping/01_make_genome_indexes.sh
@@ -1,6 +1,9 @@
 docker run -it \
-    --mount type=bind,source=/project/Data/,target=/data rnamap:latest \
+    --mount type=bind,source=/project/Data/,target=/data \
+    rnamap:latest \
     --runThreadN 8 \
     --runMode genomeGenerate \
     --genomeDir /data/Fasta/STARIndices \
-    --genomeFastaFiles /data/Fasta/EquCab2/EquCab2.fa
+    --genomeFastaFiles /data/Fasta/EquCab2/EquCab2.fa \
+    --sjdbGTFfile /data/GFFs/ref_EquCab2.0_top_level.gff3 \
+    --sjdbGTFtagExonParentTranscript Parent
